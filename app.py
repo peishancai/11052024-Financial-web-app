3660cba96523b74c03b0bcc4bc92828ed5330929
peishancai <K2321107D@e.ntu.edu.sg>
2024-05-11 14:59:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index(): 
     return(render_template("index.html"))
@@ -8,6 +6,15 @@ def index():
 def main(): 
     r = request.form.get("q") 
     return(render_template("main.html",r=r)) #front end = back end
+
+@app.route("/prediction",methods=["GET","POST"])
+def prediction(): 
+    return(render_template("prediction.html",r=r))
+
+@app.route("/dbs_price",methods=["GET","POST"])
+def dbs_price(): 
+    q = float(request.form.get("q")) 
+    return(render_template("dbs_price.html",r=(q*-50.6)+90.2))
 
 if __name__=="__main__":
     app.run()
